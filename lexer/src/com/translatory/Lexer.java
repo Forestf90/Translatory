00924c4f095893005880779a2a6ab1935fbca04e
Forestf <michalsliwa96@wp.pl>
2019-11-02 22:47:20 +0100

    Detect numbers

--- lexer/src/com/translatory/Lexer.java
+++ lexer/src/com/translatory/Lexer.java
@@ -8,57 +8,78 @@ public class Lexer {
     private List<Token> tokenList = new ArrayList<>();
     private int currentIndex = 0;
 
-    public boolean analyse(String line){
+    public boolean analyse(String line) {
 
-        while(currentIndex < line.length()){
+        while (currentIndex < line.length()) {
 
-            if(b(line)){
-                if(!tokenList.isEmpty() && tokenList.get(tokenList.size()-1).getType().equals(TokenType.whiteSpace)){
-                    Token last =tokenList.get(tokenList.size()-1);
-                    last.setValue(last.getValue()+line.charAt(currentIndex));
+            if (b(line)) {
+                if (!tokenList.isEmpty() && tokenList.get(tokenList.size() - 1).getType().equals(TokenType.whiteSpace)) {
+                    Token last = tokenList.get(tokenList.size() - 1);
+                    last.setValue(last.getValue() + line.charAt(currentIndex));
+                    currentIndex++;
+                } else {
+                    tokenList.add(new Token(TokenType.whiteSpace, currentIndex, String.valueOf(line.charAt(currentIndex))));
                     currentIndex++;
                 }
-                else{
-                    tokenList.add(new Token(TokenType.whiteSpace ,currentIndex, String.valueOf(line.charAt(currentIndex))));
-                    currentIndex++;
-                }
 
-            }
-            else if(o(line)){
-                tokenList.add(new Token(TokenType.operator ,currentIndex, String.valueOf(line.charAt(currentIndex))));
+            } else if (o(line)) {
+                tokenList.add(new Token(TokenType.operator, currentIndex, String.valueOf(line.charAt(currentIndex))));
                 currentIndex++;
-            }
-            else if(n(line)){
-                tokenList.add(new Token(TokenType.bracket ,currentIndex, String.valueOf(line.charAt(currentIndex))));
+            } else if (n(line)) {
+                tokenList.add(new Token(TokenType.bracket, currentIndex, String.valueOf(line.charAt(currentIndex))));
                 currentIndex++;
+            } else if (l(line)) {
+                if (!tokenList.isEmpty()) {
+                    Token last = tokenList.get(tokenList.size() - 1);
+                    if (last.getValue().equals("-")) {
+                        last.setValue(last.getValue() + line.charAt(currentIndex));
+                        last.setType(TokenType.number);
+                        currentIndex++;
+                    } else if (last.getValue().equals("0")) {
+                        System.out.println("Unexpected character at " + currentIndex + " index");
+                        System.out.println("Multi-digit number cannot start with 0");
+                        return false;
+                    } else if (last.getType().equals(TokenType.number)) {
+                        last.setValue(last.getValue() + line.charAt(currentIndex));
+                        currentIndex++;
+                    } else {
+                        tokenList.add(new Token(TokenType.number, currentIndex, String.valueOf(line.charAt(currentIndex))));
+                        currentIndex++;
+                    }
+                }
+            } else {
+                System.out.println("Unexpected character at " + currentIndex + " index");
+                return false;
             }
-            else currentIndex++;
         }
 
         displayTokens();
         return true;
     }
 
-    public void displayTokens(){
+    public void displayTokens() {
 
-        for(Token t: tokenList){
-            System.out.println(t.getType()+" '"+t.getValue()+ "'  index:"+t.getIndex());
+        for (Token t : tokenList) {
+            System.out.println(t.getType() + " '" + t.getValue() + "'  index:" + t.getIndex());
         }
     }
-    private boolean b(String line){
 
-        //System.out.println(line.toCharArray()[currentIndex]);
+    private boolean b(String line) {
         return line.charAt(currentIndex) == ' ';
     }
 
-    private boolean o(String line){
-
-        return "+-*/".contains(""+line.charAt(currentIndex));
+    private boolean o(String line) {
+        return "+-*/".contains("" + line.charAt(currentIndex));
     }
 
-    private boolean n(String line){
+    private boolean n(String line) {
 
-        return ")(".contains(""+line.charAt(currentIndex));
+        return ")(".contains("" + line.charAt(currentIndex));
     }
 
+    private boolean l(String line) {
+
+        return "0123456789".contains("" + line.charAt(currentIndex));
+    }
 }
+
--- lexer/src/com/translatory/Main.java
+++ lexer/src/com/translatory/Main.java
@@ -5,7 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Lexer lexer = new Lexer();
-        lexer.analyse("(5 *   4) - 9");
+//        Lexer lexer = new Lexer();
+//        lexer.analyse("(5 *   4) - 9");
+
+
+        Lexer lexer2 = new Lexer();
+        lexer2.analyse("(-23532 ** 0 543)  /*55");
     }
 }
